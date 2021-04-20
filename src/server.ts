61c7f18b8eaf1705940602d7a358420871d5ddfe
Victor <victor.as.nascimento@gmail.com>
2021-04-20 00:23:29 -0300

    api started

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Hello World"
+    });
+});
+
+app.post("/users      ", (request, response) => {
+    return response.json({
+        message: "User saved!"
+    });
+});
+
+app.listen(8080, () => console.log("Server is running on port 8080"));
